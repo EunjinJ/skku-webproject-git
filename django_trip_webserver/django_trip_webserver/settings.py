e7c2c47837ea6311ad1787da9b747a8aedfc367a
earthnaut <earthnaut999@gmail.com>
2023-10-12 16:52:16 +0900

    database connect

--- django_trip_webserver/django_trip_webserver/settings.py
+++ django_trip_webserver/django_trip_webserver/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "board.apps.BoardConfig",
+    "main.apps.MainConfig",
+    "user.apps.UserConfig",
 ]
 
 MIDDLEWARE = [
@@ -74,11 +77,16 @@ WSGI_APPLICATION = "django_trip_webserver.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    "default":{
+        "ENGINE" : "django.db.backends.mysql",
+        "NAME" : "django_trip_webserver",
+        "USER" : "root",
+        "PASSWORD" : '9999',
+        "HOST" : '127.0.0.1',
+        "PORT" : '9999'
     }
 }
+
 
 
 # Password validation
