358694d33d89dc9a0bc3aadd4d6bf904fbc08bd8
earthnaut <earthnaut999@gmail.com>
2023-10-14 13:40:49 +0900

    search app html connect

--- django_trip_webserver/search/views.py
+++ django_trip_webserver/search/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 여행지 검색
+def search_trip(request):
+    return render(request, 'search/search_trip.html', {})
+
+# 여행지 댓글 검색
+def search_trip_comment(request):
+    return render(request, 'search/search_trip_comment.html', {})
+
+# 여행지 리뷰 검색
+def search_review(request):
+    return render(request, 'search/search_review.html', {})
+
+# 여행지 리뷰 댓글 검색
+def search_review_comment(request):
+    return render(request, 'search/search_review_comment.html', {})
