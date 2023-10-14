68401cd42a92bd87d4d5942eb18e56b8e4fe05a2
earthnaut <earthnaut999@gmail.com>
2023-10-14 14:32:52 +0900

    user app - login

--- django_trip_webserver/user/views.py
+++ django_trip_webserver/user/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # 로그인
 def login(request):
     return render(request, 'user/login.html', {})
+
+def signup(request):
+    return render(request, 'user/signup.html', {})
