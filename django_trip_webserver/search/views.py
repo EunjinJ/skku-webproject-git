7a1e80986f68608ca9c98902e0e1a65525db907a
earthnaut <earthnaut999@gmail.com>
2023-10-15 14:55:12 +0900

    search - trip htmly

--- django_trip_webserver/search/views.py
+++ django_trip_webserver/search/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 
-# 여행지 검색
-def search_trip(request):
-    return render(request, 'search/search_trip.html', {})
+# 상세검색 (여행지검색+여행지댓글검색+여행지리뷰검색+여행지리뷰댓글검색)
+def search(request):
+    return render(request, 'search/search.html', {})
 
-# 여행지 댓글 검색
-def search_trip_comment(request):
-    return render(request, 'search/search_trip_comment.html', {})
+# # 여행지 검색
+# def search_trip(request):
+#     return render(request, 'search/search_trip.html', {})
 
-# 여행지 리뷰 검색
-def search_review(request):
-    return render(request, 'search/search_review.html', {})
+# # 여행지 댓글 검색
+# def search_trip_comment(request):
+#     return render(request, 'search/search_trip_comment.html', {})
 
-# 여행지 리뷰 댓글 검색
-def search_review_comment(request):
-    return render(request, 'search/search_review_comment.html', {})
+# # 여행지 리뷰 검색
+# def search_review(request):
+#     return render(request, 'search/search_review.html', {})
+
+# # 여행지 리뷰 댓글 검색
+# def search_review_comment(request):
+#     return render(request, 'search/search_review_comment.html', {})
