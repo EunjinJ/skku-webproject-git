fdf0a34ee51733db99edfdab737cfb722222cf8b
GitJYoung <unbgil88@skkukdp.re.kr>
2023-10-16 01:30:27 +0900

    main html 지도 삽입

--- django_trip_webserver/main/urls.py
+++ django_trip_webserver/main/urls.py
@@ -4,4 +4,5 @@ from . import views # 현재 경로에 있는 views.py를 가져옴
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('main_seoul', views.main_seoul, name='seoul'),
 ]
--- django_trip_webserver/main/views.py
+++ django_trip_webserver/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # 메인화면(홈)
 def main(request):
     return render(request, 'main/main.html', {})
+
+# 메인화면(홈-서울)
+def main_seoul(request):
+    return render(request, 'main/main_seoul.html', {})
