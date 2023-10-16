ed184145f164f9dd889e057bd1aa9f53d2eeab3b
glaehfdl1654 <glaehfdl1654@naver.com>
2023-10-16 18:11:03 +0900

    모델 수정, migrate 다시 해주세요

--- django_trip_webserver/board/urls.py
+++ django_trip_webserver/board/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from . import views # 현재 경로에 있는 views.py를 가져옴
 
+app_name = 'board'
+
 urlpatterns = [
     path('trip/', views.trip_list, name='trip_list'),
     path('trip/<int:trip_id>', views.trip_detail, name='trip_detail'),
     path('trip/<int:trip_id>/review', views.review_list, name='review_list'),
-    path('trip/<int:trip_id>/review/<int:review_id>', views.review_list, name='review_detail'),
-    path('trip/<int:trip_id>/review/write', views.review_list, name='review_write'),
+    path('trip/<int:trip_id>/review/<int:review_id>', views.review_detail, name='review_detail'),
+    path('trip/<int:trip_id>/review/write', views.review_write, name='review_write'),
 ]
